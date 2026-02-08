af6a136406aa13292c95d6bf3db4acd274b87075
Mira <mirax@mordorx.xyz>
2026-02-08 13:25:36 +0100

    Add functionless but pretty web page

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,32 @@ mod cli;
 mod escape;
 mod quadlet;
 mod serde;
+#[macro_use] extern crate rocket;
+
+const INDEX_HTML: &str = include_str!("index.html");
 
 use clap::Parser;
 use color_eyre::eyre;
-
+use rocket::response::content::RawHtml;
 use self::cli::Cli;
 
-fn main() -> eyre::Result<()> {
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/")]
+fn index() -> RawHtml<&'static str> {
+    return RawHtml(INDEX_HTML)
+}
+
+#[launch]
+fn webmain() -> _ {
+    rocket::build().mount("/", routes![index, hello])
+}
+
+/*fn main() -> eyre::Result<()> {
     color_eyre::install()?;
 
     Cli::parse().print_or_write_files()
-}
+}*/
